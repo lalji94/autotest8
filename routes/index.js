4a4150c0ada62ed122d089e9ef86b56f93e858cd
lalji94 <63184003+lalji94@users.noreply.github.com>
2020-09-12 22:14:36 +0530

    Update index.js
    api add time wise tag change

--- routes/index.js
+++ routes/index.js
@@ -476,6 +476,37 @@ router.post('/tagChangePostFlags', function (req, res) {
   });
 });
 
+router.post('/tagChangePostAmazon', function (req, res) {
+  async.waterfall([
+    function (nextCall) {
+      values =  [
+                   req.body.org_post_tag
+                ]
+      var sqlss = "UPDATE post_flags set org_post_tag =? WHERE id = 1";
+      connection.query(sqlss, values, function (err, rides) {
+        if (err) {
+          return nextCall({
+            "message": "something went wrong",
+          });
+        }
+        nextCall(null, rides[0]);
+      })
+    }
+  ], function (err, response) {
+    if (err) {
+      return res.send({
+        status: err.code ? err.code : 400,
+        message: (err && err.msg) || "someyhing went wrong"
+      });
+    }
+    return res.send({
+      status: 200,
+      message: "Edit post flag update sucessfully",
+      data: response
+    });
+  });
+});
+
 router.post('/editFlipkartFlags', function (req, res) {
   async.waterfall([
     function (nextCall) {
