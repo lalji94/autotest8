a61fb77559a446853eb0ac788f4ed31d223f2d87
lalji94 <63184003+lalji94@users.noreply.github.com>
2020-09-26 17:34:26 +0530

    Update index.js
    api upload

--- routes/index.js
+++ routes/index.js
@@ -179,6 +179,35 @@ router.get('/telegram', function (req, res, next) {
   })
 });
 
+router.post('/bitlyChangePostAmzn', function (req, res) {
+  async.waterfall([
+    function (nextCall) {
+      values =  [
+                   req.body.org_post_tag
+                ]
+      var sql = "UPDATE post_flags set current_bitly =? WHERE id = 1";
+      connection.query(sqlss, values, function (err, rides) {
+        if (err) {
+          return nextCall({
+            "message": "something went wrong",
+          });
+        }
+        nextCall(null, rides[0]);
+      })
+    }
+  ], function (err, response) {
+    if (err) {
+      return res.send({
+        status: err.code ? err.code : 400,
+        message: (err && err.msg) || "someyhing went wrong"
+      });
+    }
+    return res.send({
+      status: 200,
+      message: "Edit post flag update sucessfully",
+      data: response
+    });
+  });
 router.get('/telegram_posts', function (req, res, next) {
   async.waterfall([
     function (nextCall) {
