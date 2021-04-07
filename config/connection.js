5491b63efdcb4b0727441356a6dd1301b8061972
lalji94 <63184003+lalji94@users.noreply.github.com>
2021-04-07 21:54:58 +0530

    Update connection.js
    db change

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,18 @@ var mysql  = require('mysql');
 
 
 //- Connection configuration
+// var db_config = {
+//   host     : 'remotemysql.com',
+//   user     : 'LEUJq5pWf1',
+//   password : '6Pm2yLz9Wy',
+//   database:'LEUJq5pWf1'
+// };
+
 var db_config = {
-  host     : 'remotemysql.com',
-  user     : 'LEUJq5pWf1',
-  password : '6Pm2yLz9Wy',
-  database:'LEUJq5pWf1'
+  host     : 'sql6.freesqldatabase.com',
+  user     : 'sql6403808',
+  password : 'FYvV7W3WAL',
+  database:'sql6403808'
 };
 
 //- Create the connection variable
